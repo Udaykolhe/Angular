6a2494afc35c73d5c999545893577c6820cac23d
Udaykolhe <udaykolhe03@gmail.com>
2023-08-17 17:43:11 +0530

    Validation & CheckBox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup , Validators } from "@angular/forms";
+import { FormControl, FormGroup, Validators, FormArray } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -9,23 +9,50 @@ import { FormControl, FormGroup , Validators } from "@angular/forms";
 export class AppComponent {
   title = 'reactive_form';
 
+  hobbiesArray: string[] = ['Cricket', 'FootBall', 'Pool'];
+
+
 
   signupForm = new FormGroup({
-      name : new FormControl('' ,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
-      age : new FormControl('' , [Validators.required,Validators.min(10),Validators.max(50)]),
-      email : new FormControl('' , [Validators.required,Validators.pattern('^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})')]),
-      pass : new FormControl('',[Validators.required,Validators.pattern('^([a-zA-Z0-9@*#]{8,15})$')])  
-    });
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    age: new FormControl('', [Validators.required, Validators.min(10), Validators.max(50)]),
+    email: new FormControl('', [Validators.required, Validators.pattern('^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})')]),
+    pass: new FormControl('', [Validators.required, Validators.pattern('^([a-zA-Z0-9@*#]{8,15})$')]),
+    gender: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+    accept: new FormControl(false, Validators.requiredTrue),
+    hobbies: new FormArray([], [Validators.required])
+
+  });
 
   //Email :-  ^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$
   // Password :- (?=^.{8,}$)((?=.*\d)|(?=.\*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$
   //^([a-zA-Z0-9@*#]{8,15})$
 
-  handleSubmit(){
-   console.log(this.signupForm.value); 
+  onChange(e: any) {
+    let selectedValue = e.target.value;
+    const checked = e.target.checked;
+    console.log(selectedValue, checked)
+
+    const checkedArray = this.signupForm.get('hobbies') as FormArray;
+    if (checked) {
+      checkedArray.push(new FormControl(selectedValue));
+    } else {
+      let i: number = 0;
+      checkedArray.controls.forEach((item) =>{
+            if(item == selectedValue){
+              checkedArray.removeAt(i);
+            }
+            i++;
+      });
+    }
   }
-get f(){
-  return this.signupForm.controls;
-}
+
+  handleSubmit() {
+    console.log(this.signupForm.value);
+  }
+  get f() {
+    return this.signupForm.controls;
+  }
 
 }
