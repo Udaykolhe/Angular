68466e91d91e05e209c890ab9a944319fcc7303f
Udaykolhe <udaykolhe03@gmail.com>
2023-08-17 16:28:15 +0530

    validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup , Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -11,14 +11,21 @@ export class AppComponent {
 
 
   signupForm = new FormGroup({
-      name : new FormControl(''),
-      age : new FormControl(''),
-      email : new FormControl('')
-  });
+      name : new FormControl('' ,[Validators.required,Validators.minLength(3),Validators.maxLength(20)]),
+      age : new FormControl('' , [Validators.required,Validators.min(10),Validators.max(50)]),
+      email : new FormControl('' , [Validators.required,Validators.pattern('^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})')]),
+      pass : new FormControl('',[Validators.required,Validators.pattern('^([a-zA-Z0-9@*#]{8,15})$')])  
+    });
+
+  //Email :-  ^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z])*@([0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$
+  // Password :- (?=^.{8,}$)((?=.*\d)|(?=.\*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$
+  //^([a-zA-Z0-9@*#]{8,15})$
 
   handleSubmit(){
    console.log(this.signupForm.value); 
   }
-
+get f(){
+  return this.signupForm.controls;
+}
 
 }
